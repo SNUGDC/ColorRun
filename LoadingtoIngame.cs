2c3e2c9bda5e6838c8063567a4fe24a07f611f44
aayumin <35535321+aayumin@users.noreply.github.com>
2018-01-22 18:31:04 +0900

    UI 2주차부분 아주조금
    시작화면이랑 로딩창 하려고했는데 로딩창에서 넘어가는게 안되긴하네요...ㅠㅠ

---
+++ LoadingtoIngame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingtoIngame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int timecount = 50;
+		timecount = timecount - 1;
+		print (timecount);
+		if (timecount < 0){
+			SceneManager.LoadScene (2);
+		}
+	}
+
+	public void Loading_to_Ingame() {
+
+		int timecount = 50;
+		while (timecount >= 0) {
+			timecount = timecount - 1;
+		}
+		print (timecount);
+		if (timecount < 0) {
+			SceneManager.LoadScene (2);
+		}
+	}
+
+
+
+}

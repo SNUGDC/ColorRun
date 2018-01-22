fe8006f23063caf0987af5b1923b93aeb6ff46a8
Myeongzin Son <sonmyungzin1@naver.com>
2018-01-22 19:54:03 +0900

    유민씨의 로딩 스크립트를 수정

---
+++ Assets/LoadingToIngame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingToIngame : MonoBehaviour {
+
+	int timecount = 50;
+	public string sceneName;
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+
+	void Update()
+	{
+		timecount = timecount - 1;
+		Debug.Log(timecount);
+		if (timecount < 0){
+			SceneManager.LoadScene ("InGame");
+		}
+	}
+}

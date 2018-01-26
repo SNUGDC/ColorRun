d2570c4225949e011018d8c411c223b73afcce0c
Eom-PC\Eom <esh0821@naver.com>
2018-01-26 23:03:54 +0900

    유니티가 8시간 업데이트하네요..

---
+++ Assets/TrafficLightsScripts.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLightsScripts : MonoBehaviour {
+
+    //Traffic means Trafficlights
+    public float ScrollSpeed = 5.0f;
+    public GameObject[] Traffic;
+    public float frequency = 0.5f;
+    float counter = 0.0f;
+    public Transform TrafficSpawnPoint;
+
+    // Use this for initialization
+    void Start () {
+        GenerateRandomTraffic();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //GenerateTrafficlights
+        if (counter<=0.0f)
+        {
+            GenerateRandomTraffic();
+            
+        }
+        else
+        {
+            counter -= Time.deltaTime * frequency;
+        }
+
+        //scrolling
+        GameObject currentChild;
+        for(int i=0; i<transform.childCount; i++)
+        {
+            currentChild = transform.GetChild(i).gameObject;
+            ScrollTraffic(currentChild);
+            if(currentChild.transform.position.x<=-15.0f)
+            {
+                Destroy(currentChild);
+            }
+
+        }
+
+	}
+
+    void ScrollTraffic(GameObject currentTraffic)
+    {
+        currentTraffic.transform.position -= Vector3.right * (ScrollSpeed * Time.deltaTime);
+    }
+
+    void GenerateRandomTraffic()
+    {
+        GameObject  newTraffic = Instantiate(Traffic[Random.Range(0, Traffic.Length)], TrafficSpawnPoint.position, Quaternion.identity) as GameObject;
+        newTraffic.transform.parent = transform;
+        counter = 1.0f;
+    }
+
+}

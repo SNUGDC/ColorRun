f20664b3680450ca7bbe481d4af6a5315ca73874
변다빈 <bdv951101@gmail.com>
2018-01-27 01:08:15 +0900

    레인을 세 개로 추가, 플레이어가 키보드 위아래 화살표를 누르면 이동하도록 함.

--- Assets/PlayerScripts.cs
+++ Assets/PlayerScripts.cs
@@ -8,9 +8,20 @@ public class PlayerScripts : MonoBehaviour {
 	void Start () {
 		
 	}
-	
+
+	public float speed;
+
 	// Update is called once per frame
 	void Update () {
-		
+		MoveControl ();
+	}
+
+	void MoveControl(){
+		if (Input.GetKeyDown (KeyCode.UpArrow) && (transform.position.y <= 0 )) {
+			transform.position += Vector3.up * speed;
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow) && (transform.position.y >= 0 )) {
+			transform.position += Vector3.down * speed;
+		}
 	}
 }

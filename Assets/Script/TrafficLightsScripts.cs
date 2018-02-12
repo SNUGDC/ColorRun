dc20058e01b4d0327b6ed08d46e8748dc6f4ae3c
변다빈 <bdv951101@gmail.com>
2018-02-12 17:29:05 +0900

    고수민 씨가 올려주셨던 '플레이어 속도 및 신호등 간격 조절 변화'를 적용했습니다. 이제 신호등이 다가오는 속도가 갈수록 빨라집니다. 이를 이용한 버닝 상태 구현을 곧 시도할 예정입니다.

--- Assets/Script/TrafficLightsScripts.cs
+++ Assets/Script/TrafficLightsScripts.cs
@@ -4,58 +4,94 @@ using UnityEngine;
 
 public class TrafficLightsScripts : MonoBehaviour {
 
-    //Traffic means Trafficlights
-    public float scrollSpeed = 5.0f;
-    public GameObject[] Traffic;
+	//Traffic means Trafficlights
+	public float scrollSpeed = 5.0f;
+	public GameObject[] Traffic;
 	public GameObject trafficLight;
-    public float frequency = 0.5f;
-    float counter = 0.0f;
-    public Transform TrafficSpawnPoint;
+	public float frequency = 0.3f;
+	float counter = 0.0f;
+	public Transform TrafficSpawnPoint;
+	float x = 0.0f;
+	float y = 1.6f;
+	public float z = 0.2f;
+	int n = 1;
 
-    // Use this for initialization
-    void Start () {
-        GenerateRandomTraffic();
+
+	// Use this for initialization
+	void Start () {
+		GenerateRandomTraffic();
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
-        //GenerateTrafficlights
-        if (counter<=Random.Range(0.0f, 0.2f))
-        {
-            GenerateRandomTraffic();
-            
-        }
-        else
-        {
-            counter -= Time.deltaTime * frequency;
-        }
+		float k = Mathf.Log(x, y) + 5;
+		if (k <= 0.0f)
+		{
+			scrollSpeed = 5.0f;
+			frequency = 0.3f;
+			x += 1 * Time.deltaTime;
+		}
+		else
+		{
+			scrollSpeed = Mathf.Log(x, y) + 5;
+			frequency = Mathf.Log(scrollSpeed, 3.0f) - 1.2f;
+			x += 1 * Time.deltaTime;
+		}
 
-        //scrolling
-        GameObject currentChild;
-        for(int i=0; i<transform.childCount; i++)
-        {
-            currentChild = transform.GetChild(i).gameObject;
-            ScrollTraffic(currentChild);
-            if(currentChild.transform.position.x<=-15.0f)
-            {
-                Destroy(currentChild);
-            }
 
-        }
+		//GenerateTrafficlights
+		z += 0.0025f * Time.deltaTime;
+
+		if (z < 0.5)
+		{
+			if (counter <= Random.Range(0.0f, z))
+			{
+				GenerateRandomTraffic();
+				n += 1;
+				Debug.Log(n);
+
+			}
+			else
+			{
+				counter -= Time.deltaTime * frequency;
+			}
+
+		}
+		else
+		{
+			z -= 0.1f;
+		}
+
+
+		//scrolling
+		GameObject currentChild;
+		for(int i=0; i<transform.childCount; i++)
+		{
+			currentChild = transform.GetChild(i).gameObject;
+			ScrollTraffic(currentChild);
+			if(currentChild.transform.position.x<=-15.0f)
+			{
+				Destroy(currentChild);
+			}
+
+		}
 
 	}
 
-    void ScrollTraffic(GameObject currentTraffic)
-    {
-        currentTraffic.transform.position -= Vector3.right * (scrollSpeed * Time.deltaTime);
-    }
+	void ScrollTraffic(GameObject currentTraffic)
+	{
+		currentTraffic.transform.position -= Vector3.right * (scrollSpeed * Time.deltaTime);
+	}
 
-    void GenerateRandomTraffic()
-    {
-        GameObject  newTraffic = Instantiate(trafficLight, TrafficSpawnPoint.position, Quaternion.identity) as GameObject;
-        newTraffic.transform.parent = transform;
-        counter = 1.0f;
-    }
+	void GenerateRandomTraffic()
+	{
+		GameObject  newTraffic = Instantiate(trafficLight, TrafficSpawnPoint.position, Quaternion.identity) as GameObject;
+		newTraffic.transform.parent = transform;
+		counter = 1.0f;
+	}
+
+
 
 }

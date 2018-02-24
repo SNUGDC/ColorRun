7adc2933e8b63f4dba5cab24609ce2a5a7b0c6ee
Eom-PC\Eom <esh0821@naver.com>
2018-02-24 17:04:57 +0900

    AudioMixer 이용해보려했는데 잘 안되네요..

---
+++ Assets/Script/ChangeMusicVolumeInSetting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeMusicVolumeInSetting : MonoBehaviour {
+
+    // Use this for initialization
+
+    public Slider Volume;
+    public AudioSource myMusic;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myMusic.volume = Volume.value;
+
+    }
+}

be8f13d9e0c28deaf863209a05813e911489fe8a
변다빈 <bdv951101@gmail.com>
2018-02-25 17:41:19 +0900

    현재 도전목표와 정확히 일치하는 상태일 때, 다음 도전목표를 제시하도록 변경. 한 게임 내에서 빨랐다가 느려지면, 가장 빨랐을 때의 속도가 도전과제에 제대로 저장되지 않는 문제 해결.

--- Assets/Script/UserData.cs
+++ Assets/Script/UserData.cs
@@ -37,7 +37,7 @@ public class UserData : MonoBehaviour {
 	void NextSumScore(){
 		PV.sumScore = PlayerPrefs.GetInt ("SumScore", 0);
 		PV.nextSumScore = PlayerPrefs.GetInt ("NextSumScore", 0);
-		if (PV.sumScore > PV.nextSumScore) {
+		if (PV.sumScore >= PV.nextSumScore) {
 			n1 = n1 + 1;
 			PV.nextSumScore = 5000 * (int)Mathf.Pow(2, n1);
 			PlayerPrefs.SetInt ("NextSumScore", PV.nextSumScore);
@@ -47,7 +47,7 @@ public class UserData : MonoBehaviour {
 	void NextBestScore(){
 		PV.bestScore = PlayerPrefs.GetInt ("BestScore", 0);
 		PV.nextBestScore = PlayerPrefs.GetInt ("NextBestScore", 0);
-		if (PV.bestScore > PV.nextBestScore) {
+		if (PV.bestScore >= PV.nextBestScore) {
 			n2 = n2 + 1;
 			PV.nextBestScore = 1000 * n2;
 			PlayerPrefs.SetInt ("NextBestScore", PV.nextBestScore);
@@ -57,7 +57,7 @@ public class UserData : MonoBehaviour {
 	void NextTotalGreenLights(){
 		PV.totalGreenLights = PlayerPrefs.GetInt ("TotalGreenLights", 0);
 		PV.nextTotalGreenLights = PlayerPrefs.GetInt ("NextTotalGreenLights", 0);
-		if (PV.totalGreenLights > PV.nextTotalGreenLights) {
+		if (PV.totalGreenLights >= PV.nextTotalGreenLights) {
 			n3 = n3 + 1;
 			PV.nextTotalGreenLights = 125 * (int)Mathf.Pow(2, n3);
 			PlayerPrefs.SetInt ("NextTotalGreenLights", PV.nextTotalGreenLights);
@@ -67,7 +67,7 @@ public class UserData : MonoBehaviour {
 	void NextComboGreenLight(){
 		PV.comboGreenLight = PlayerPrefs.GetInt ("ComboGreenLight", 0);
 		PV.nextComboGreenLight = PlayerPrefs.GetInt ("NextComboGreenLight", 0);
-		if (PV.comboGreenLight > PV.nextComboGreenLight) {
+		if (PV.comboGreenLight >= PV.nextComboGreenLight) {
 			n4 = n4 + 1;
 			PV.nextComboGreenLight = 10 * n4;
 			PlayerPrefs.SetInt ("NextComboGreenLight", PV.nextComboGreenLight);
@@ -77,7 +77,7 @@ public class UserData : MonoBehaviour {
 	void NextSumBurningCount(){
 		PV.sumBurningCount = PlayerPrefs.GetInt ("SumBurningCount", 0);
 		PV.nextSumBurningCount = PlayerPrefs.GetInt ("NextSumBurningCount", 0);
-		if (PV.sumBurningCount > PV.nextSumBurningCount) {
+		if (PV.sumBurningCount >= PV.nextSumBurningCount) {
 			n5 = n5 + 1;
 			PV.nextSumBurningCount = 5 * n5;
 			PlayerPrefs.SetInt ("NextSumBurningCount", PV.nextSumBurningCount);
@@ -87,7 +87,7 @@ public class UserData : MonoBehaviour {
 	void NextSumGetItem(){
 		PV.sumGetItem = PlayerPrefs.GetInt ("SumGetItem", 0);
 		PV.nextSumGetItem = PlayerPrefs.GetInt ("NextSumGetItem", 0);
-		if (PV.sumGetItem > PV.nextSumGetItem) {
+		if (PV.sumGetItem >= PV.nextSumGetItem) {
 			n6 = n6 + 1;
 			PV.nextSumGetItem = 15 * n6;
 			PlayerPrefs.SetInt ("NextSumGetItem", PV.nextSumGetItem);
@@ -97,7 +97,7 @@ public class UserData : MonoBehaviour {
 	void NextBestSpeed(){
 		PV.bestSpeed = PlayerPrefs.GetFloat ("BestSpeed", 0);
 		PV.nextBestSpeed = PlayerPrefs.GetFloat ("NextBestSpeed", 0);
-		if (PV.bestSpeed > PV.nextBestSpeed) {
+		if (PV.bestSpeed >= PV.nextBestSpeed) {
 			n7 = n7 + 1;
 			PV.nextBestSpeed = 10 + (5 * n7);
 			PlayerPrefs.SetFloat ("NextBestSpeed", PV.nextBestSpeed);
@@ -107,7 +107,7 @@ public class UserData : MonoBehaviour {
 	void NextTotalTouch(){
 		PV.totalTouch = PlayerPrefs.GetInt ("TotalTouch", 0);
 		PV.nextTotalTouch = PlayerPrefs.GetInt ("NextTotalTouch", 0);
-		if (PV.totalTouch > PV.nextTotalTouch) {
+		if (PV.totalTouch >= PV.nextTotalTouch) {
 			n8 = n8 + 1;
 			PV.nextTotalTouch = 100 * (int)Mathf.Pow(2, n8);
 			PlayerPrefs.SetInt ("NextTotalTouch", PV.nextTotalTouch);

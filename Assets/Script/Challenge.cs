ce79e91c2f3d3d3018eac6388ccc0c442fc9736a
변다빈 <bdv951101@gmail.com>
2018-02-25 10:42:45 +0900

    현재와 목표 수치가 메인메뉴에서 도전과제 씬으로 들어가면 늘 뜨게 함. 나머지 도전과제 6개의 데이터만 구현하면 제가 이번주에 맡았던 부분은 끝납니다.

---
+++ Assets/Script/Challenge.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Challenge : MonoBehaviour {
+
+	public GameObject totalDistanceUI;
+	public GameObject nextTotalDistanceUI;
+	public GameObject bestDistanceUI;
+	public GameObject nextBestDistanceUI;
+	public GameObject totalGreenLightsUI;
+	public GameObject nextTotalGreenLightsUI;
+	public GameObject comboGreenLightUI;
+	public GameObject nextComboGreenLightUI;
+	public GameObject burningUI;
+	public GameObject nextBurningUI;
+	public GameObject getItemUI;
+	public GameObject nextGetItemUI;
+	public GameObject bestSpeedUI;
+	public GameObject nextBestSpeedUI;
+	public GameObject totalTouchUI;
+	public GameObject nextTotalTouchUI;
+	PlayerValue PV;
+
+	void Awake()
+	{
+		PV = FindObjectOfType<PlayerValue>();
+	}
+
+	void Start () {
+		Load ();
+
+		totalDistanceUI.GetComponent<Text> ().text = PV.sumScore + "m";
+		nextTotalDistanceUI.GetComponent<Text> ().text = PV.nextSumScore + "m";
+		bestDistanceUI.GetComponent<Text> ().text = PV.bestScore + "m";
+		nextBestDistanceUI.GetComponent<Text> ().text = PV.nextBestScore + "m";
+	}
+		
+	void Update () {
+		
+	}
+
+	void Load() {
+		PV.sumScore = PlayerPrefs.GetInt ("SumScore", 0);
+		PV.nextSumScore = PlayerPrefs.GetInt ("NextSumScore", 0);
+		PV.bestScore = PlayerPrefs.GetInt ("BestScore", 0);
+		PV.nextBestScore = PlayerPrefs.GetInt ("NextBestScore", 0);
+	}
+}
